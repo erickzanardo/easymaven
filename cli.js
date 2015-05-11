f75d71d10d332c1c59108f58136b6db4776d90b8
Rodrigo Navarro <rnavarro1@gmail.com>
2015-05-11 14:33:08 -0300

    Multi platform path join.
    As we are talking about Java, it makes sense to proper support Windows.

--- cli.js
+++ cli.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var path = require('path');
 var easymaven = require('./index.js');
 
 var args = process.argv;
 var cwd = process.cwd();
 
-var pomPath = [cwd, 'pom.xml'].join('/');
-
 if(args.length != 3) {
   console.log('Wrong usage, you must inform the artifact, example "easymaven gson"');
 } else {
+  var pomPath = path.join(cwd, 'pom.xml');
+
   fs.exists(pomPath, function(exists) {
    if(exists) {
      var artifact = args[2]
